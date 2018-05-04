e3c743871df49576ff244f82e5dd1271beb359f2
Muktadirul <raiancse@gmail.com>
2018-05-04 23:38:46 +0600

    Updated

--- Final.sh
+++ Final.sh
@@ -1,15 +1,15 @@
-#java 8 install
+#====================== java 8 install ====================== 
 echo -ne '\n' | sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update -y
 sudo apt-get install oracle-java8-installer -y
-#system upgrade
 
+#====================== system upgrade ====================== 
 sudo apt-get install update -y
 sudo apt-get install upgrade-system -y
 sudo apt-get install g++ -y
 sudo apt-get dist-upgrade -y
 
-#VLC Gimp wall paper
+#====================== VLC Gimp wall paper ====================== 
 sudo apt-get install vlc -y 
 sudo apt-get install gimp -y
 sudo apt-get install gimp-data -y
@@ -17,8 +17,7 @@ sudo apt-get install gimp-plugin-registry -y
 sudo apt-get install gimp-data-extras -y
 sudo apt-get install  ubuntu-wallpapers* -y
 
-#Cleaning
-
+#====================== Cleaning ====================== 
 echo "Cleaning Up" &&
 sudo apt-get -f install &&
 sudo apt-get autoremove &&
@@ -26,79 +25,47 @@ sudo apt-get -y autoclean &&
 sudo apt-get -y clean -y
 
 #Code Blocks
-sudo apt-get install codeblocks -y
+#sudo apt-get install codeblocks -y
 
-#Chrome
-if [[ $(getconf LONG_BIT) = "64" ]]
-then
-	echo "64bit Detected" &&
-	echo "Installing Google Chrome" &&
-	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &&
-	sudo dpkg -i google-chrome-stable_current_amd64.deb &&
-	rm -f google-chrome-stable_current_amd64.deb
-else
-	echo "32bit Detected" &&
-	echo "Installing Google Chrome" &&
-	wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb &&
-	sudo dpkg -i google-chrome-stable_current_i386.deb &&
-	rm -f google-chrome-stable_current_i386.deb
-fi
-
-
-#operamini
+#====================== Browser =================================
+#====================== Chrome =================================
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i --force-depends google-chrome-stable_current_amd64.deb -y
+sudo apt-get install -f -y
+#====================== operamini ====================== 
 sudo sh -c 'echo "deb http://deb.opera.com/opera/ stable non-free" >> /etc/apt/sources.list.d/opera.list'
 sudo sh -c 'wget -O - http://deb.opera.com/archive.key | apt-key add -'
 sudo apt-get update -y
 sudo apt-get install opera -y
-#chromium
+#====================== chromium ====================== 
 sudo apt-get install chromium-browser -y
-#Sublime3
+
+# ====================== Sublime3 ====================== 
 sudo add-apt-repository ppa:webupd8team/sublime-text-3 -y
 sudo apt-get update -y
 sudo apt-get install sublime-text-installer -y
-#mongodb
-echo -ne '\n' | sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
-sudo apt-get update -y
-sudo apt-get install -y mongodb-enterprise -y
-#flashplayer
+
+#====================== flashplayer ====================== 
 sudo apt-get install pepperflashplugin-nonfree -y
 sudo update-pepperflashplugin-nonfree --install -y
-#Cpufreq Indicator
+
+#====================== Cpufreq Indicator ====================== 
 sudo apt-get install indicator-cpufreq -y
-#download manager
-sudo add-apt-repository ppa:sikon/steadyflow -y
-sudo apt-get update -y
-sudo apt-get install steadyflow -y
-#rar unrar 
+
+#====================== rar unrar ====================== 
 sudo apt-get install rar unrar-y
-#pycharm
-echo -ne '\n' | sudo add-apt-repository ppa:viktor-krivak/pycharm
-sudo apt-get update -y
-sudo apt-get install pycharm-professional -y
-#pymongo
-pip install pymongo
-#pip
-pip3 install --upgrade pip
-#avro
+
+# ====================== avro ====================== 
 wget "https://github.com/maateen/avro/releases/download/v2.0/avro_2.0-1_all.deb"
 sudo dpkg -i avro_2.0-1_all.deb -y
 sudo apt-get install -f
 ibus restart
-#Jupyter Notebook
+# ====================== Jupyter Notebook ====================== 
 pip3 install jupyter
-#git
+# ====================== git ====================== 
 sudo apt-get install git -y
-#pandas
-pip install pandas
-#scrapy
-pip install scrapy
-#numpy
-pip install numpy
-#firefox
 
-
-
-#Others
+#====================== Others ====================== 
 sudo apt-get install indicator-cpufreq -y
 echo -ne '\n' | sudo apt-add-repository ppa:fixnix/netspeed
 echo -ne '\n' | sudo add-apt-repository ppa:noobslab/indicators
@@ -107,7 +74,8 @@ sudo apt-get install indicator-netspeed-unity indicator-netspeed-unity -y
 sudo apt-get install indicator-weather -y
 sudo apt-get install copyq -y
 sudo apt-get install gnome-system-monitor -y
-#Removing downloaded data
+
+#====================== Removing downloaded data ====================== 
 echo "Cleaning Up" &&
 sudo apt-get -f install &&
 sudo apt-get autoremove &&
