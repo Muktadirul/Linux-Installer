50e5632a20559b44a4618e03ea63a90fe17fe560
Muktadirul <raiancse@gmail.com>
2018-05-05 02:14:05 +0600

    multi locad added

--- Config Ubuntu.sh
+++ Config Ubuntu.sh
@@ -27,6 +27,10 @@ echo -ne '\n' | sudo add-apt-repository ppa:gnome-terminator
 sudo apt-get update -y
 sudo apt-get install terminator -y
 
+#====================== Multiload ======================
+echo -ne '\n' | sudo add-apt-repository ppa:indicator-multiload/stable-daily
+sudo apt-get update -y
+sudo apt-get install indicator-multiload -y
 
 #====================== Weather ======================
 
@@ -138,10 +142,6 @@ sudo apt-get install copyq -y
 #====================== Genom system  Monitor ======================
 sudo apt-get install gnome-system-monitor -y
 
-#====================== Weather Indicator ======================
-echo -ne '\n' | sudo add-apt-repository ppa:atareao/atareao
-sudo apt-get update -y
-sudo apt-get install my-weather-indicator -y
 #====================== all packages ======================
 sudo apt-get install synaptic vlc gimp gimp-data gimp-plugin-registry gimp-data-extras y-ppa-manager bleachbit openjdk-7-jre oracle-java8-installer flashplugin-installer unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller libxine1-ffmpeg mencoder flac faac faad sox ffmpeg2theora libmpeg2-4 uudeview libmpeg3-1 mpeg3-utils mpegdemux liba52-dev mpeg2dec vorbis-tools id3v2 mpg321 mpg123 libflac++6 totem-mozilla icedax lame libmad0 libjpeg-progs libdvdcss2 libdvdread4 libdvdnav4 libswscale-extra-2 ubuntu-restricted-extras ubuntu-wallpapers* -y
 
